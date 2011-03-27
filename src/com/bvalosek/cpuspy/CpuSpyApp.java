e0c025a5397901b1ccebdaee111d5bade157fa72
Brandon Valosek <bvalosek@gmail.com>
2011-03-26 22:23:11 -0500

    read in cpu states time file

--- src/com/bvalosek/cpuspy/CpuSpyApp.java
+++ src/com/bvalosek/cpuspy/CpuSpyApp.java
@@ -13,9 +13,47 @@ package com.bvalosek.cpuspy;
 
 // imports
 import android.app.Application;
+import android.util.Log;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 /** main application class */
 public class CpuSpyApp extends Application {
 
+   public static final String TIME_IN_STATE_PATH =
+      "/sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state";
+
+   /** get the time-in-states info */
+   public Map<Integer, Integer> getTimeInStates () {
+      Map<Integer, Integer> states = new LinkedHashMap<Integer, Integer>();
+
+      try {
+         // create a buffered reader to read in the time-in-states log
+         InputStream is = new FileInputStream (TIME_IN_STATE_PATH);
+         InputStreamReader ir = new InputStreamReader (is);
+         BufferedReader br = new BufferedReader (ir);
+
+         // read in the lines
+         String line;
+         while ( (line = br.readLine ()) != null ) {
+            // split open line and convert to Integers
+            String[] nums = line.split (" ");
+            states.put (Integer.parseInt(nums[0]), Integer.parseInt(nums[1]) );
+         }
+
+         is.close ();
+
+      } catch (Exception e) {
+         Log.e ("cpuspy", e.getMessage() );
+         return null;
+      }
+
+      // made it
+      return states;
+   }
 
 }
--- src/com/bvalosek/cpuspy/ui/HomeActivity.java
+++ src/com/bvalosek/cpuspy/ui/HomeActivity.java
@@ -1,3 +1,13 @@
+//-----------------------------------------------------------------------------
+// cpuspy
+//
+// (c) Brandon Valosek, 2011 <bvalosek@gmail.com>
+//
+// HomeActivity.java
+//
+// Main UI
+//-----------------------------------------------------------------------------
+
 package com.bvalosek.cpuspy.ui;
 
 // my stuff
@@ -6,14 +16,35 @@ import com.bvalosek.cpuspy.*;
 // imports
 import android.app.Activity;
 import android.os.Bundle;
+import java.util.Map;
+import android.util.Log;
 
+/** main activity class */
 public class HomeActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+   /** talk to the app */
+   private CpuSpyApp mApp = null;
+
+   /** Called when the activity is first created. */
+   @Override public void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+
+      // inflate the view
+      setContentView(R.layout.main);
+
+      // get app context
+      mApp = (CpuSpyApp)getApplicationContext ();
+
+      // get proc
+      Map<Integer,Integer> states = mApp.getTimeInStates ();
+
+      for (Map.Entry<Integer,Integer> entry : states.entrySet() ) {
+         int freq = entry.getKey();
+         int time = entry.getValue ();
+
+         Log.d ("cpuspy", "freq=" + freq + " time=" + time);
+      }
+
+   }
 }
