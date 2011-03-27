ccd2e5a3f5213dfc4b6feb477b0e488dc4e8bfad
Brandon Valosek <bvalosek@gmail.com>
2011-03-26 22:53:48 -0500

    added listview

--- src/com/bvalosek/cpuspy/ui/HomeActivity.java
+++ src/com/bvalosek/cpuspy/ui/HomeActivity.java
@@ -19,6 +19,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import java.util.List;
+import android.widget.ListView;
 
 /** main activity class */
 public class HomeActivity extends Activity
@@ -26,16 +27,22 @@ public class HomeActivity extends Activity
    /** talk to the app */
    private CpuSpyApp mApp = null;
 
+   /** list of states */
+   ListView mUiStateList = null;
+
    /** Called when the activity is first created. */
    @Override public void onCreate(Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
 
       // inflate the view
-      setContentView(R.layout.main);
+      setContentView(R.layout.home_layout);
 
       // get app context
       mApp = (CpuSpyApp)getApplicationContext ();
+
+      // find views
+      mUiStateList = (ListView)findViewById (R.id.ui_states_list);
 
       // get proc
       List<CpuState> states = mApp.getTimeInStates ();
