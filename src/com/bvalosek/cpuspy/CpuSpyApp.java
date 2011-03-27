4008f424f1c949f9ef5b1d6230ab6ec2c654fcfc
Brandon Valosek <bvalosek@gmail.com>
2011-03-26 19:41:10 -0500

    added in custom app class

---
+++ src/com/bvalosek/cpuspy/CpuSpyApp.java
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------------
+// cpuspy
+//
+// (c) Brandon Valosek, 2011 <bvalosek@gmail.com>
+//
+// CpuSpyApp.java
+//
+// Main application class
+//
+//-----------------------------------------------------------------------------
+
+package com.bvalosek.cpuspy;
+
+// imports
+import android.app.Application;
+
+/** main application class */
+public class CpuSpyApp extends Application {
+
+
+}
