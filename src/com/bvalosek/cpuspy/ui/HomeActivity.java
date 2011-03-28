d012d7b36234e9d3f8db122b3220a0ee056e8993
Brandon Valosek <bvalosek@gmail.com>
2011-03-28 00:16:34 -0500

    added menu and warning for no states

--- src/com/bvalosek/cpuspy/ui/HomeActivity.java
+++ src/com/bvalosek/cpuspy/ui/HomeActivity.java
@@ -17,15 +17,18 @@ import com.bvalosek.cpuspy.CpuSpyApp.CpuState;
 // imports
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ListView;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.ScrollView;
 import android.widget.ProgressBar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import java.util.List;
+import java.util.ArrayList;
 
 /** main activity class */
 public class HomeActivity extends Activity
@@ -33,11 +36,20 @@ public class HomeActivity extends Activity
    /** talk to the app */
    private CpuSpyApp mApp = null;
 
-   /** view of the list of states */
-   ListView mUiStateList = null;
-
    /** where we dump the states views */
-   LinearLayout mUiStatesView = null;
+   private LinearLayout mUiStatesView = null;
+
+   /** additional states text */
+   private TextView mUiAdditionalStates = null;
+
+   /** additional states haeder */
+   private TextView mUiHeaderAdditionalStates = null;
+
+   /** warning for no states */
+   private TextView mUiStatesWarning = null;
+
+   /** kernel string */
+   private TextView mUiKernelString = null; 
 
    /** Called when the activity is first created. */
    @Override public void onCreate(Bundle savedInstanceState)
@@ -52,26 +64,68 @@ public class HomeActivity extends Activity
 
       // find views
       mUiStatesView = (LinearLayout)findViewById (R.id.ui_states_view);
+      mUiKernelString = (TextView)findViewById (R.id.ui_kernel_string);
+      mUiAdditionalStates = (TextView)findViewById (R.id.ui_additional_states);
+      mUiHeaderAdditionalStates = (TextView)findViewById (
+         R.id.ui_header_additional_states);
+      mUiStatesWarning = (TextView)findViewById (R.id.ui_states_warning);
 
       // draw all the info
-      updateDate ();
+      updateData ();
+   }
+
+   /** resume */
+   @Override public void onResume () {
+      super.onResume ();
       updateView ();
    }
 
    /** update the view */
    public void updateView () {
-
-      // FILL IT UP
+      /* fill up the layout with views for the states, recording which ones
+       * are empty */
+      mUiStatesView.removeAllViews();
+      List<String> extraStates = new ArrayList<String> ();
       for (CpuState state : mApp.getStates () ) {
-         generateStateRow (state, mUiStatesView);
+         if (state.duration > 0) {
+            generateStateRow (state, mUiStatesView);
+         } else {
+            extraStates.add (state.freq/1000 + " MHz");
+         }
       }
 
+      // no states?!
+      if ( mApp.getStates().size() == 0) {
+      	mUiStatesWarning.setVisibility (View.VISIBLE);
+      }
+
+      // for all empty views, edit the additional line textview
+      if (extraStates.size() > 0) {
+         int n = 0;
+         String str = ""; 
+         // construct nice looking comma-seperated list
+         for (String s : extraStates) {
+            if (n++ > 0)
+               str += ", ";
+            str += s;
+         }
+         mUiAdditionalStates.setVisibility(View.VISIBLE);
+         mUiHeaderAdditionalStates.setVisibility (View.VISIBLE);
+         mUiAdditionalStates.setText (str);
+      }
+
+
+      // kernel line
+      mUiKernelString.setText ( mApp.getKernelString () );
    }
 
    /** update the data */
    public void updateData () {
       // get the time spent in states
       mApp.updateTimeInStates ();
+      
+      // from /proc/version
+      mApp.updateKernelString ();
    }
 
    /** spit out a view representing a cpustate so we can cram it into a ScrollView */
@@ -84,8 +138,17 @@ public class HomeActivity extends Activity
       // what percetnage we've got
       float per = (float)state.duration * 100 / mApp.getTotalStateTime (); 
 
-      // pretty freq string
+      // pretty strings
       String sFreq = state.freq / 1000 + " MHz";
+      String sPer = (int)per + "%";
+      int h = (int)Math.floor (state.duration / 6000);
+      int m = (state.duration / 100) % 60;
+      String sDur;
+      if (m < 10)
+      	sDur = h + ":0" + m;
+      else
+      	sDur = h + ":" + m;
+   
 
       // map UI elements to objects
       TextView freqText = (TextView)theRow.findViewById(R.id.ui_freq_text);
@@ -95,13 +158,40 @@ public class HomeActivity extends Activity
 
       // modify the row
       freqText.setText (sFreq);
-      perText.setText (Float.toString (per) );
-      durText.setText (Integer.toString(state.duration));
+      perText.setText (sPer);
+      durText.setText (sDur);
       bar.setProgress ( (int)per);
 
       // add it to parent and return
       parent.addView(theRow);
    	return theRow;
    }
+
+
+   /** called when we want to infalte the menu */
+   @Override public boolean onCreateOptionsMenu (Menu menu) {
+      // request inflater from activity and inflate into its menu
+      MenuInflater inflater = getMenuInflater ();
+      inflater.inflate (R.menu.home_menu, menu);
+
+      // made it
+      return true;
+   }
+
+   /** called to handle a menu event */
+   @Override public boolean onOptionsItemSelected (MenuItem item) {
+      // what it do mayne
+      switch (item.getItemId () ) {
+      /* pressed the load menu button */
+      case R.id.menu_refresh:
+         updateData ();
+         updateView ();
+         break;
+      }
+
+      // made it
+      return true;
+   }
+
 
 }
