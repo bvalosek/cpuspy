69be84ed89fbe6935e2a5c186c23d5c81b4391f8
Brandon Valosek <bvalosek@gmail.com>
2011-03-27 21:26:34 -0500

    added in reading from /proc/version

--- src/com/bvalosek/cpuspy/CpuSpyApp.java
+++ src/com/bvalosek/cpuspy/CpuSpyApp.java
@@ -27,12 +27,22 @@ public class CpuSpyApp extends Application {
    public static final String TIME_IN_STATE_PATH =
       "/sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state";
 
+   public static final String VERSION_PATH = "/proc/version";
+
    /** array of the states / duration */
    private List<CpuState> mStates = new ArrayList<CpuState>();
+
+   /** kernel build string */
+   private String mKernelString = "";
 
    /** access state list */
    public List<CpuState> getStates () {
    	return mStates;
+   }
+
+   /** access kerenl string*/
+   public String getKernelString () {
+      return mKernelString;
    }
 
    /** get the total state time */
@@ -52,7 +62,34 @@ public class CpuSpyApp extends Application {
       public int freq = 0;
       public int duration = 0;
    }
-   
+  
+   /** get the kernel string */
+   public String updateKernelString () {
+      try {
+         // create a buffered reader to read in the time-in-states log
+         InputStream is = new FileInputStream (VERSION_PATH);
+         InputStreamReader ir = new InputStreamReader (is);
+         BufferedReader br = new BufferedReader (ir);
+
+         // clear out the array and read in the new state lines
+         String line;
+         while ( (line = br.readLine ()) != null ) {
+         	// nop
+         }
+
+         is.close ();
+
+      } catch (Exception e) {
+         Log.e ("cpuspy", e.getMessage() );
+         return null;
+      }
+
+      mKernelString = line;
+      
+      // made it
+      return mKernelString;
+   }
+
    /** get the time-in-states info */
    public List<CpuState> updateTimeInStates () {
       try {
