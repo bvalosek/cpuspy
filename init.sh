e7b6e2e5e1628416627a4d8c39734c437af6697b
Brandon Valosek <bvalosek@gmail.com>
2011-12-06 04:01:30 -0600

    added init script to initialize the Android project
    Mainly for use when developing from the command line

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This sets up the source directory for use with the Android SDK tools from
+# the command line.
+
+
+# Run from the root directory of the project
+
+android update project \
+    -p . \
+    -n cpuspy \
+    -t android-8
